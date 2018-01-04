97cd7f411953e5616087b6070738a110b596789d
Daniel Schep <dschep@gmail.com>
2018-01-03 22:31:43 -0500

    rewrite hello to use lambda-decorators

--- handler.py
+++ handler.py
@@ -1,24 +1,12 @@
-import json
+from lambda_decorators import dump_json_body
 
 
+@dump_json_body
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "statusCode": 200,
+        "body": {
+            "message": "Go Serverless v1.0! Your function executed successfully!",
+            "input": event,
+        },
     }
-    """
